56d55ccace823b6aea8dcfec62cec5a70bf40ca6
Boroshilov03 <boroshilov.mirlan@gmail.com>
2024-06-28 12:59:27 -0700

    Remove .env file from the repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-const App = () => {
-  return (
-    <div className=''>App</div>
-  )
-}
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
-export default App
+const App = () => {
+  const user = false;
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
+    </div>
+  );
+};
+
+export default App;
